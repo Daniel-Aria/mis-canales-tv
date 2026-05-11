5b7ebdc9a14300cc831b4f1f45910aca07b5accc
Daniel Arias <135355294+Daniel-Aria@users.noreply.github.com>
2026-05-11 19:16:57 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,7 @@ import re
 # ============================================================
 
 canales_a_buscar = {
-"TVO Canal 23 SV": "https://tvocanal23.com/tvo-en-vivo/",
-    "ABC 7 New York": "https://famelack.com/tv/ca/Xrqwoq40lkwai9",
+"TVO Canal 23 SV": "https://tvocanal23.com/tvo-en-vivo/"
 }
 # ============================================================
 
