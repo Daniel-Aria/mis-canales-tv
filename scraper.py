14956327417ac02fe9c640cc306ba7685ee9e8e3
Daniel Arias <135355294+Daniel-Aria@users.noreply.github.com>
2026-07-30 14:19:55 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import re
 # ============================================================
 
 canales_a_buscar = {
-"TVO Canal 23 SV": "https://tvocanal23.com/tvo-en-vivo/"
+"https://teleon.tv/sv/live-tv/cnn-en-espanol-sv/"
 }
 # ============================================================
 
